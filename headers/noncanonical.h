ccf54eff2429343e0344e8450f1b4cdc74b92a51
2dukes <ruipinto02@hotmail.com>
2020-10-25 19:26:47 +0000

    Added comments to header files.

--- headers/noncanonical.h
+++ headers/noncanonical.h
@@ -14,6 +14,66 @@
 #include "macros.h"
 #include "generalFunctions.h"
 
+/*-------------------------- Data Link Layer --------------------------*/
+
+/*
+* Faz o destuffing de uma mensagem.
+* Data Link Layer
+*/
+int deStuffing(unsigned char * message, int size, unsigned char* receiveMessage, off_t* receiveMessageSize);
+
+/*
+* Configura a porta de série, lê a trama de controlo SET e envia a trama UA.
+* Data Link Layer
+*/
+void llopen(int fd, struct termios* oldtio, struct termios* newtio);
+
+/*
+* Guarda a mensagem atual lida no vetor "geral" da mensagem lida.
+* Data Link Layer
+*/
+void saveMessage(unsigned char* messageRead, off_t* messageReadSize, unsigned char* currentMessage, off_t currentMessageSize);
+
+/*
+* Lê tramas I.
+* Data Link Layer
+*/
+off_t llread(int fd, int numPackets, unsigned char* messageRead);
+
+/*
+* Recebe os diferentes bytes da trama.
+* Data Link Layer
+*/
 int receiveTrama(int* nTrama, int fd, unsigned char* receivedMessage, off_t* receivedMessageSize);
 
-off_t llread(int fd, int numPackets, unsigned char* messageRead);
+/*
+* Lê trama de controlo DISC, envia DISC de volta e recebe UA.
+* Data link Layer
+*/
+void llclose(int fd);
+
+/*-------------------------- Application Layer --------------------------*/
+
+/*
+* Faz o destuffing de uma mensagem.
+* Application Layer
+*/
+bool checkEnd(unsigned char* endMessage, off_t endMessageSize);
+
+/*
+* Obtém o tamanho do ficheiro a partir da trama START.
+* Application Layer
+*/
+off_t sizeOfFile_Start(unsigned char *start);
+
+/*
+* Obtém o nome do ficheiro a partir da trama START.
+* Application Layer
+*/
+unsigned char *nameOfFile_Start(unsigned char *start);
+
+/*
+* Cria ficheiro com os dados recebidos nas tramas I.
+* Application Layer
+*/
+void createFile(unsigned char *data, off_t* sizeFile, unsigned char filename[]);
--- headers/writenoncanonical.h
+++ headers/writenoncanonical.h
@@ -15,10 +15,67 @@
 #include "macros.h"
 #include "generalFunctions.h"
 
+/*-------------------------- Data Link Layer --------------------------*/
+
+/*
+* Aciona o Timeout da trama de controlo SET.
+* Data Link Layer
+*/
 void alarmHandler(int sigNum);
 
+/*
+* Aciona o Timeout da trama de controlo DISC.
+* Data Link Layer
+*/
+void resendDISC(int signum);
+
+/*
+* Aciona o Timeout da trama I.
+* Data Link Layer
+*/
 void resendTrama(int sigNum);
 
+/*
+* Faz o stuffing dos bytes de informação.
+* Data Link Layer
+*/
+int byteStuffing(unsigned char* dataToSend, int size);
+
+/*
+* Configura a porta de série, envia a trama de controlo SET e recebe a trama UA.
+* Data Link Layer
+*/
 void llopen(struct termios* oldtio, struct termios* newtio);
 
-void llwrite(unsigned char* buf, off_t size);
+/*
+* Envia as tramas I.
+* Data link layer
+*/
+void llwrite(unsigned char *buf, off_t size);
+
+/*
+* Envia a trama de controlo DISC, recebe DISC de volta e envia UA.
+* Data link Layer
+*/
+void llclose();
+
+/*-------------------------- Application Layer --------------------------*/
+
+/*
+* Abre um ficheiro e lê o seu conteúdo.
+* Application layer
+*/
+unsigned char* openReadFile(char* fileName, off_t* fileSize);
+
+/*
+* Constrói os pacotes de controlo com o tamanho do ficheiro e o nome do mesmo.
+* Application layer
+*/
+unsigned char* buildControlTrama(char* fileName, off_t* fileSize, unsigned char controlField, off_t* packageSize);
+
+/*
+* Encapsula a mensagem a enviar para ficar de acordo com os pacotes de dados.
+* Application layer
+*/
+unsigned char* encapsulateMessage(unsigned char* messageToSend, off_t* messageSize, off_t* totalMessageSize, int* numPackets);
+

ef629ca4c0228b435a0371211dc2184e14117ccc
2dukes <ruipinto02@hotmail.com>
2020-11-20 18:43:27 +0000

    Parsed URL arguments

--- Project2/Part1/src/clientTCP.c
+++ Project2/Part1/src/clientTCP.c
@@ -10,11 +10,55 @@
 #include <signal.h>
 #include <netdb.h>
 #include <strings.h>
+#include <string.h>
 
 #define SERVER_PORT 6000
 #define SERVER_ADDR "192.168.28.96"
+#define MAX_STRING_LENGTH 64
+
+void parseArguments(char* argv, char* user, char* password, char* host, char* urlPath) {
+	// User
+	printf("%s\n", argv);
+	char* colon = strchr(&argv[4], ':');
+	int colonIndex = colon - argv;
+	memcpy(user, &argv[6], colonIndex - 6);
+	user[colonIndex - 6] = '\0';
+	printf("User: %s\n", user);
+
+	// Password
+	char* at = strchr(argv, '@');
+	int atIndex = at - argv;
+	memcpy(password, &argv[colonIndex + 1], atIndex - (colonIndex + 1));
+	password[atIndex - (colonIndex + 1)] = '\0';
+	printf("Password: %s\n", password);
+
+	// Host
+	char* hostSlash = strchr(&argv[6], '/');
+	int slashIndex = hostSlash - argv;
+	memcpy(host, &argv[atIndex + 1], slashIndex - (atIndex + 1));
+	host[slashIndex - (atIndex + 1)] = '\0';
+	printf("Host: %s\n", host);
+
+	// url-path
+	char* urlPathSlash = strchr(&argv[6], '/') + 1;
+	int pathIndex = urlPathSlash - argv;
+	int urlPathLen = strlen(argv) - pathIndex ;
+	memcpy(urlPath, &argv[slashIndex + 1], urlPathLen);
+	urlPath[urlPathLen] = '\0';
+	printf("Url Path: %s\n", urlPath);
+}
 
 int main(int argc, char** argv){
+	// ftp://[<user>:<password>@]<host>/<url-path>
+
+	char user[MAX_STRING_LENGTH];
+	char password[MAX_STRING_LENGTH];
+	char host[MAX_STRING_LENGTH]; 
+	char urlPath[MAX_STRING_LENGTH];
+
+	// url-path
+	parseArguments(argv[1], user, password, host, urlPath);
+	exit(1);
 
 	int	sockfd;
 	struct	sockaddr_in server_addr;
---
+++ Project2/Part1/src/compile.sh
@@ -0,0 +1,1 @@
+gcc -o client clientTCP.c

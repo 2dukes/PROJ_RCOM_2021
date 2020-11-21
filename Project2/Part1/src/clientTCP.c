58ce8a729b5b4127529e1476c183cd1ecca46086
2dukes <ruipinto02@hotmail.com>
2020-11-21 11:26:50 +0000

    Small Simplification.

--- Project2/Part1/src/clientTCP.c
+++ Project2/Part1/src/clientTCP.c
@@ -11,6 +11,7 @@
 #include <netdb.h>
 #include "macros.h"
 #include <string.h>
+#include <fcntl.h>
 
 
 void errorMessage(char* message, int statusCode) {
@@ -36,14 +37,11 @@ struct FTPclientArgs parseArguments(char* argv) {
 		int colonIndex = colon - argv;
 		clientArgs.user = (char *) malloc((colonIndex - URL_HEADER_LEN) + 1);
 		memcpy(clientArgs.user, &argv[URL_HEADER_LEN], colonIndex - URL_HEADER_LEN);
-		clientArgs.user[colonIndex - URL_HEADER_LEN] = '\0';
-
 
 		// Password
 		atIndex = at - argv;
 		clientArgs.password = (char *) malloc((atIndex - (colonIndex + 1)) + 1);
 		memcpy(clientArgs.password, &argv[colonIndex + 1], atIndex - (colonIndex + 1));
-		clientArgs.password[atIndex - (colonIndex + 1)] = '\0';
 	}
 	else {
 		clientArgs.user = (char *) malloc(11);
@@ -62,14 +60,12 @@ struct FTPclientArgs parseArguments(char* argv) {
 	int slashIndex = hostSlash - argv;
 	clientArgs.host = (char *) malloc((slashIndex - (atIndex + 1)) + 1);
 	memcpy(clientArgs.host, &argv[atIndex + 1], slashIndex - (atIndex + 1));
-	clientArgs.host[slashIndex - (atIndex + 1)] = '\0';
 
 	// url-path
 	int pathIndex = urlPathSlash - argv;
 	int urlPathLen = strlen(argv) - pathIndex ;
 	clientArgs.urlPath = (char *) malloc(urlPathLen + 1);
 	memcpy(clientArgs.urlPath, &argv[slashIndex + 1], urlPathLen);
-	clientArgs.urlPath[urlPathLen] = '\0';
 
 	return clientArgs;
 }
@@ -101,7 +97,11 @@ struct hostent* getIP(char* hostName)
 	return h;
 }
 
+void readResponse(int sockfd, char* statusCode) {
 
+
+}
+// https://stackoverflow.com/questions/29152359/how-to-read-multiline-response-from-ftp-server
 int main(int argc, char** argv) {
 	// ftp://[<user>:<password>@]<host>/<url-path>
 	// ftp://ftp.up.pt/pub/README.html
@@ -119,7 +119,7 @@ int main(int argc, char** argv) {
 	struct hostent* hostInfo = getIP(clientArgs.host);
 
 	int	sockfd;
-	struct	sockaddr_in server_addr;
+	struct sockaddr_in server_addr;
 	int	bytes;
 	
 	/*server address handling*/
@@ -137,7 +137,7 @@ int main(int argc, char** argv) {
 
 		struct sockaddr {
 			u_short sa_family; // Address family - ex: AF_INET
-			char sa_data[14]; // Protocol address
+			char sa_data[14]; // Protocol address - sa_data contains a destination address and port number for the socket
 		};
 	*/
 
@@ -147,25 +147,24 @@ int main(int argc, char** argv) {
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
     
 	/*open an TCP socket*/
-	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-    		perror("socket()");
-        	exit(0);
-    	}
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) 
+		errorMessage("socket() failed.", 2);
+
 	/*connect to the server*/
-    	if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
-		   sizeof(server_addr)) < 0){
-        	perror("connect()");
-		exit(0);
-	}
-    	/*send a string to the server*/
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+		errorMessage("connect() failed.", 3);
+    
+	/*send a string to the server*/
+	char statusCode[4];
+	readResponse(sockfd, statusCode);
+
 	char buf[2];
 	while(1) {
 		bytes = read(sockfd, buf, 1);
 		printf("%c | %d\n", buf[0], buf[0]);
 	}
 	
-
+	free(hostInfo);
 	free(clientArgs.user);
 	free(clientArgs.password);
 	free(clientArgs.host);
